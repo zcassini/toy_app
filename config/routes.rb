f810c05a88ebceee7d1a65bdc11bbef23f54c3be
Zac Cassini <zcassini@gmail.com>
2015-02-28 12:35:47 -0500

    Add Micropost scaffold and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+
   resources :users
    root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
